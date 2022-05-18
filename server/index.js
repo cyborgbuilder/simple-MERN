33dfb3e42a14cf66b6369a2f8d956e1b9be62b5f
cyborgbuilder <isuruud8@gmail.com>
2022-05-18 10:26:10 +0530

    ver--01

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express()
+const mongoose = require('mongoose')
+const UserModel = require("./models/users")
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+
+mongoose.connect("mongodb+srv://userudadmin:midalt123Tomain@cluster0.pfegt.mongodb.net/testDatabase?retryWrites=true&w=majority")
+
+
+app.get("/getUsers", (req, res) => {
+    UserModel.find( {}, (err, result) => {
+        if (err){
+            res.json(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+app.post("/createUser", async (req, res) => {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+
+    res.json(user);
+
+
+    
+})
+
+
+app.listen(8080, () => {
+    console.log("SERVER IS RUNNING");
+});
+
